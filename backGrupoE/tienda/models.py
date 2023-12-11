9809a7ec47904fe91a93bdd3a0a40a4d43630514
Matias Tailler <matiast86@gmail.com>
2023-12-11 20:43:02 -0300

    templates en tienda

--- backGrupoE/tienda/models.py
+++ backGrupoE/tienda/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=20, verbose_name = 'titulo')
     image = models.ImageField(verbose_name = 'imagen', upload_to="projects")
-    description = models.CharField(max_length=20, verbose_name = 'precio')
+    precio = models.IntegerField(verbose_name = 'precio')
     create = models.DateTimeField(auto_now_add=True, verbose_name = 'fecha de creacion')
     update = models.DateTimeField(auto_now=True, verbose_name = 'fecha de modificacion')
     
